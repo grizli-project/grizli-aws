67c189cb37d77d5c68a396a208b5cdc026f42995
Gabriel Brammer <brammer@stsci.edu>
2018-05-09 14:23:12 -0400

    Add sync after extraction

--- scripts/grizli_extract_and_fit.sh
+++ scripts/grizli_extract_and_fit.sh
@@ -15,20 +15,30 @@ root=$1
 
 echo "Running on root=${root}"
 
+# Clean up
 cd $HOME/GrizliExtract
 rm *GrismFL* *npy j[0-9]* *wcs*
 
+# Copy from S3
 aws s3 sync s3://aws-grivam/Pipeline/${root}/Extractions/ ./
 aws s3 cp s3://aws-grivam/Pipeline/${root}_footprint.fits ./
 
+## Extractions
 grizli_extract_and_fit.py ${root} run
 
+# Sync extractions
+aws s3 sync --exclude "*" --include "${root}_*fits" ./ s3://aws-grivam/Pipeline/${root}/Extractions/
+aws s3 sync --exclude "*" --include "${root}_*png" --include "*html" --acl public-read ./ s3://aws-grivam/Pipeline/${root}/Extractions/
+
+# Redshift fits
 cpu_count=`python -c 'import multiprocessing; print(multiprocessing.cpu_count()//2)'`
 mpiexec -n $cpu_count python -m mpi4py.futures $GRIZLICODE/grizli/pipeline/run_MPI.py
 
+# Summary HTML & zhist figure
 grizli_extract_and_fit.py ${root} summary
 
-aws s3 sync --exclude "*" --include "${root}_*fits" ./ s3://aws-grivam/Pipeline/${root}/Extractions/
-aws s3 sync --exclude "*" --include "${root}_*png" --include "*html" --acl public-read ./ s3://aws-grivam/Pipeline/${root}/Extractions/
+# Sync final
+aws s3 sync --exclude "*" --include "${root}*fits" ./ s3://aws-grivam/Pipeline/${root}/Extractions/
+aws s3 sync --exclude "*" --include "${root}*png" --include "${root}*reg" --include "*html" --acl public-read ./ s3://aws-grivam/Pipeline/${root}/Extractions/
 
 

5132ea39604700edf395f48138a726ee9a0c10ab
Gabriel Brammer <brammer@stsci.edu>
2018-05-09 14:48:51 -0400

    add sync scripts

--- setup.py
+++ setup.py
@@ -8,5 +8,7 @@ setup(name='grizli_aws',
       url='https://github.com/grizli-project/grizli-aws',
       packages=['grizli_aws'],
       scripts=['scripts/grizli_extract_and_fit.sh',
-               'scripts/grizli_extract_and_fit.py']
+               'scripts/grizli_extract_and_fit.py',
+               'scripts/sync_extractions_TO_s3',
+               'scripts/sync_extractions_FROM_s3']
      )

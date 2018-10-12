413d42112728e76df3d00ac096f5837df30f828c
Gabriel Brammer <gbrammer@gmail.com>
2018-10-12 16:08:59 +0200

    Faster log checking

--- scripts/imaging_run_all.sh
+++ scripts/imaging_run_all.sh
@@ -4,9 +4,24 @@
 
 roots=`aws s3 ls s3://grizli-imaging/Pipeline/Fields/ | grep footprint.fits | awk '{print $4}' | sed "s/_footprint.fits//"`
 
+# Existing
+aws s3 ls s3://grizli-imaging/Pipeline/Log/Start/ > /tmp/grizli_started.log
+aws s3 ls s3://grizli-imaging/Pipeline/Log/Finished/ > /tmp/grizli_finished.log
+
 date > /tmp/imaging_run_all.log
 
 for root in $roots; do
+    
+    ostart=`grep ${root}.log /tmp/grizli_started.log | awk '{print $4}'`
+    ostop=`grep ${root}.log /tmp/grizli_finished.log | awk '{print $4}'`
+    #if [[ -z $ostart && -z $ostop ]]; then echo $root; fi; done
+    
+    if [[ -n $ostart || -n $ostop ]]; then
+        echo "Skip ${root} (start=$ostart --- stop=$ostop)"
+        echo "Skip ${root} (start=$ostart --- stop=$ostop)" >> /tmp/imaging_run_all.log
+        continue
+    fi
+    
     start=`aws s3 ls s3://grizli-imaging/Pipeline/Log/Start/${root}.log | awk '{print $4}'`
     stop=`aws s3 ls s3://grizli-imaging/Pipeline/Log/Finished/${root}.log| awk '{print $4}'`
     

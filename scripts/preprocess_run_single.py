c796f14e81e7d37ee53fa1dc86c3db6f70f7a872
Gabriel Brammer <gbrammer@gmail.com>
2018-10-16 13:30:23 +0200

    Add fp region

--- scripts/preprocess_run_single.py
+++ scripts/preprocess_run_single.py
@@ -4,7 +4,7 @@ def auto_run(root='j023507-040202'):
     import os
     import matplotlib.pyplot as plt
     
-    from grizli import utils
+    from grizli import utils, prep
     from grizli.pipeline import auto_script, photoz
     utils.set_warnings()
     
@@ -17,6 +17,21 @@ def auto_run(root='j023507-040202'):
     IS_PARALLEL = utils.column_string_operation(tab['proposal_pi'], 'alkan', method='count', logical='or').sum() > 0
         
     auto_script.go(root=root, maglim=[19, 23], HOME_PATH=HOME_PATH, inspect_ramps=False, manual_alignment=False, is_parallel_field=IS_PARALLEL, reprocess_parallel=False, only_preprocess=True, run_extractions=True, run_fit=True, s3_sync=True, fine_radec=None, combine_all_filters=False, gaia_by_date=True, align_simple=False, align_clip=-1, master_radec=None, is_dash=False, run_parse_visits=True, reference_wcs_filters=['F160W','F140W','F125W','F105W','F110W','F098M','F814W','F850LP', 'F606W','F435W'], make_phot=False, make_mosaics=False, align_rms_limit=4, align_min_overlap=2)
+    
+    # Make footprints
+    files = glob.glob('*wht.fits')
+    if len(files) > 0:
+        fp = open('{0}.fp.reg'.format(root),'w')
+        fp.write('fk5\n')
+    
+        for weight_image in files:
+            root_i = '_dr'.join(file.split('_dr')[:-1])
+            reg = prep.drizzle_footprint(weight_image, shrink=10, ext=0,
+                                         outfile=None, label=root_i) 
+            fp.write(reg+'\n')
+        
+        fp.close()
+    
         
 if __name__ == "__main__":
     import sys

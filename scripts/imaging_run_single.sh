8a83868b4505df61bc34c88e17f0c043632c2aba
Gabriel Brammer <gbrammer@gmail.com>
2018-10-26 17:35:31 +0200

    Add Extractions for Grism cases

--- scripts/imaging_run_single.sh
+++ scripts/imaging_run_single.sh
@@ -37,7 +37,7 @@ rm ${root}/Prep/*bkg.fits
 rm ${root}/Prep/astrodrizzle.log
 
 # Sync extractions
-aws s3 sync --exclude "*" --include "${root}/Prep/${root}*" --include "${root}/Prep/*.log" --include "${root}/Prep/*fine*" --acl public-read ./ s3://grizli-imaging/Pipeline/
+aws s3 sync --exclude "*" --include "${root}/Prep/${root}*" --include "${root}/Prep/*.log" --include "${root}/Prep/*fine*" --include "${root}/Extractions/*" --acl public-read ./ s3://grizli-imaging/Pipeline/
 
 if [ -e ${root}/Prep/${root}_phot.fits ]; then 
     echo "${root}: Success"

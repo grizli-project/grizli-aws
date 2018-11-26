11d14cb811cf698d05c7ce2f0a38eb12b8cd705b
Gabriel Brammer <gbrammer@gmail.com>
2018-11-26 08:46:28 +0100

    Fix bug

--- scripts/preprocess_fixwcs_single.py
+++ scripts/preprocess_fixwcs_single.py
@@ -178,12 +178,12 @@ if __name__ == "__main__":
     utils.set_warnings()
             
     root = sys.argv[1]
-    auto_run(root=root) 
+    #auto_run(root=root) 
     
-    # try:
-    #     auto_run(root=root) 
-    # except:
-    #     fp = open('{0}.failed'.format(root),'w')
-    #     fp.write(time.ctime()+'\n')
-    #     fp.close()
+    try:
+        auto_run(root=root) 
+    except:
+        fp = open('{0}.failed'.format(root),'w')
+        fp.write(time.ctime()+'\n')
+        fp.close()
         

3e28c478dafb5193cf4680c13bd457b7aa28084e
Gabriel Brammer <gbrammer@gmail.com>
2018-12-04 21:40:31 +0100

    Add GAIA option for no available reference radec

--- scripts/preprocess_fixwcs_single.py
+++ scripts/preprocess_fixwcs_single.py
@@ -38,9 +38,9 @@ def auto_run(root='j023507-040202', flag_global_crs=False):
                                   
     # Redrizzle mosaics
     prep.drizzle_overlaps(visits, check_overlaps=False, skysub=False,
-                          static=False, pixfrac=0.8, scale=None, 
+                          static=False, pixfrac=0.5, scale=None, 
                           final_wcs=False, fetch_flats=False, final_rot=None,
-                          include_saturated=True)
+                          include_saturated=False)
     
     ####### Alignment
     os.system('rm *wcs.*')

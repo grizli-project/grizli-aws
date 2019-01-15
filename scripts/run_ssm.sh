e2b77dd8bcdf9de843599fbd7236dc66ce56e175
Gabriel Brammer <gbrammer@gmail.com>
2019-01-15 22:41:19 +0100

    don't sync existing

--- scripts/run_ssm.sh
+++ scripts/run_ssm.sh
@@ -1,11 +1,19 @@
 aws ec2  describe-instances --filters "Name=instance-state-name,Values=running" --query "Reservations[*].Instances[*].[InstanceId,InstanceType]" | sort
 
-ids=`aws ec2  describe-instances --filters "Name=instance-state-name,Values=running" --query "Reservations[*].Instances[*].[InstanceId,InstanceType,PublicDnsName]" | grep large| awk '{print $1}' | sort`
+ids=`aws ec2  describe-instances --filters "Name=instance-state-name,Values=running" --query "Reservations[*].Instances[*].[InstanceId,InstanceType,PublicDnsName]" | sort -k 1 |  grep c5d | awk '{print $1}' `
 
-ids=`aws ec2  describe-instances --filters "Name=instance-state-name,Values=running" --query "Reservations[*].Instances[*].[InstanceId]" | awk '{print $1}' | sort `
+
+### Drizzler
+id=i-0a8bae4cfe82dbce5
+aws ec2 start-instances --instance-id $id --profile oliveraws
+aws ec2 describe-instances --instance-id $id --profile oliveraws --query "Reservations[*].Instances[*].[InstanceId,InstanceType,PublicDnsName]"
+
+
+ids=`aws ec2  describe-instances --filters "Name=instance-state-name,Values=running" --query "Reservations[*].Instances[*].[InstanceType,InstanceId,PublicDnsName]" | sort -k 2 | awk '{print $2}' `
 
 # aws ssm send-command --document-name "AWS-RunShellScript" --instance-ids "${id}" --parameters '{"commands":["auto_run_preprocess_single cos-j1001p0217-f140w-022"],"executionTimeout":["172000"]}' --timeout-seconds 600 --region us-east-1
 
+# Refresh all repos
 for id in $ids; do     
     echo $id
     
@@ -13,19 +21,28 @@ for id in $ids; do
 
 done
 
+init=""
+
+init="\"refresh_repositories > /tmp/refresh.log; aws s3 sync s3://grizli-preprocess/Scripts/ /usr/local/bin/ >> /tmp/refresh.log; chmod +x /usr/local/bin/auto_run*\","
+
 for id in $ids; do     
     echo $id
     
     #aws ssm send-command --document-name "AWS-RunShellScript" --instance-ids "${id}" --parameters '{"commands":["auto_run_spectral_fits","halt"],"executionTimeout":["172000"]}' --timeout-seconds 600 --region us-east-1
+    
+    ########### Run pipeline on each sub-field
     #aws ssm send-command --document-name "AWS-RunShellScript" --instance-ids "${id}" --parameters '{"commands":["auto_run_preprocess","halt"],"executionTimeout":["172000"]}' --timeout-seconds 600 --region us-east-1
-    aws ssm send-command --document-name "AWS-RunShellScript" --instance-ids "${id}" --parameters '{"commands":["auto_run_preprocess_fixwcs","halt"],"executionTimeout":["172000"]}' --timeout-seconds 600 --region us-east-1
+    
+    ############# Run fixwcs
+    aws ssm send-command --document-name "AWS-RunShellScript" --instance-ids "${id}" --parameters "{\"commands\":[${init}\"auto_run_preprocess_fixwcs\",\"halt\"],\"executionTimeout\":[\"172000\"]}" --timeout-seconds 600 --region us-east-1
+    
     #aws ssm send-command --document-name "AWS-RunShellScript" --instance-ids "${id}" --parameters '{"commands":["auto_run_grism","halt"],"executionTimeout":["172000"]}' --timeout-seconds 600 --region us-east-1
     #aws ssm send-command --document-name "AWS-RunShellScript" --instance-ids "${id}" --parameters '{"commands":["auto_run_preprocess_single j1000p0206-f606w-005"],"executionTimeout":["14400"]}' --timeout-seconds 600 --region us-east-1
 
     #aws ssm send-command --document-name "AWS-RunShellScript" --instance-ids "${id}" --parameters '{"commands":["auto_fit_redshift_local j0332m2743","halt"],"executionTimeout":["21600"]}' --timeout-seconds 600 --region us-east-1
 
 
-    sleep 5 # Force asynchronous
+    sleep 8 # Force asynchronous
 
 done
 

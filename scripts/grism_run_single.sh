8ec94df0a05db93bb6141966259dfb0a76c23132
Gabriel Brammer <gbrammer@gmail.com>
2019-01-21 12:25:11 +0100

    Add command line args

--- scripts/grism_run_single.sh
+++ scripts/grism_run_single.sh
@@ -38,7 +38,7 @@ aws s3 cp s3://${BUCKET}/Pipeline/Fields/${root}_parent.radec ./
 
 if [ $2 == "-sync" ]
   then
-    aws s3 sync --exclude "*" --include "Prep/${root}*sci.fits.gz" s3://${BUCKET}/Pipeline/${root} ./
+    aws s3 sync --exclude "*" --include "Prep/${root}*sci.fits.gz" s3://${BUCKET}/Pipeline/${root}/ ./${root}/
     gunzip ${root}/Prep/*fits.gz
 fi
 

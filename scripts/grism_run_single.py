7a376860b99216f6e337982271ec1bebe33a3fd2
Gabriel Brammer <gbrammer@gmail.com>
2019-05-06 18:04:40 +0200

    rework call for new kwargs

--- scripts/grism_run_single.py
+++ scripts/grism_run_single.py
@@ -6,55 +6,61 @@ def auto_run(root='j023507-040202', args=[]):
     utils.set_warnings()
     from grizli.pipeline import auto_script
     
-    params = {
-    # Preprocessing flags         
-    's3_sync': True,               # Fetch data from AWS
-    'inspect_ramps': False,        # Visual inspection on ramps
-    'remove_bad':True,             # Remove bad EXPFLAG visits
-    'reprocess_parallel': True,    # Reprocess ramps in parallel
-    'is_dash': False,              # Flag if visits are in DASH mode
-    'filters': auto_script.VALID_FILTERS,   #  Only use these filters
-    'run_parse_visits': True,      # Parse visits to _visits.npy
-    'combine_minexp':2,            # Try to combine visits with 2 or fewer exp
-    'is_parallel_field': False,    # If parallels, change visit parsing
-    'skip_single_optical_visits': False, # Skip preprocess if single exp
-    'fix_stars': True,             # Fill DQ flagged centers of stars
-    'imaging_bkg_params': {'bh': 128, 'bw': 128, 'fh': 3, 'fw': 3,
-                       'pixel_scale': 0.06}, # Imaging background 
-
-    # Alignment
-    'manual_alignment': False,    # Interactive alignment
-    'align_clip': 120,            # Parameter for initial alignment pairs
-    'align_mag_limits': [14, 24], # Magnitude range for alignment sources
-    'align_min_overlap': 0.2,     # Overlap fraction to use HST catalogs
-    'align_outlier_threshold': 4, # Parameter for triangle matches
-    'align_rms_limit': 2,    # Force shifts = 0 if rms > rms limit
-    'align_simple': False,   # ??
-    'catalogs': ['PS1','DES','NSC','SDSS','GAIA','WISE'], # Ref cat order
-    'gaia_by_date': True,   # Get GAIA catalogs by visit epoch
-    'master_radec': None,    # Force reference
-    'parent_radec': None,    # Reference to use if no HST overlaps
-
-    'run_fine_alignment': False, # Run Fine alignment script
-    'fine_radec': None,         # Fine alingment reference,  else GAIA
-
-    # Image Mosaics                   
-    'make_mosaics': True,             # Make full-field mosaics
-    'fill_mosaics': 'grism',          # Fill empty parts of IR mosaics
-    'mask_spikes': False,             # Mask IR diffraction spikes
-    'combine_all_filters': False,      # Combine ACS & IR
-    'mosaic_pixel_scale': None,       # Pixel scale of mosaics (0.06)
-    'half_optical_pixscale': False,   # Make smaller ACS/UVIS pixel scale
-    'mosaic_pixfrac': 0.75,           # Pixfrac of mosaic images
-    'reference_wcs_filters': auto_script.VALID_FILTERS, # These define WCS if they exist
-    'make_phot': True,                # Make photometric catalog
-
-    # Spectral Extractions
-    'only_preprocess': False, # Make grism models
-    'run_extractions': False, # Don't extract grism spectra
-    'maglim': [17, 26],       # Magnitude range of grism spectra 
-    'run_fit': False          # Fit grism spectra
-    }
+    kwargs = auto_script.get_yml_parameters() 
+    
+    kwargs['fetch_files_args']['reprocess_parallel'] = True
+    kwargs['preprocess_args']['skip_single_optical_visits'] = False
+    kwargs['run_fine_alignment'] = False
+    
+    # params = {
+    # # Preprocessing flags         
+    # 's3_sync': True,               # Fetch data from AWS
+    # 'inspect_ramps': False,        # Visual inspection on ramps
+    # 'remove_bad':True,             # Remove bad EXPFLAG visits
+    # 'reprocess_parallel': True,    # Reprocess ramps in parallel
+    # 'is_dash': False,              # Flag if visits are in DASH mode
+    # 'filters': auto_script.VALID_FILTERS,   #  Only use these filters
+    # 'run_parse_visits': True,      # Parse visits to _visits.npy
+    # 'combine_minexp':2,            # Try to combine visits with 2 or fewer exp
+    # 'is_parallel_field': False,    # If parallels, change visit parsing
+    # 'skip_single_optical_visits': False, # Skip preprocess if single exp
+    # 'fix_stars': True,             # Fill DQ flagged centers of stars
+    # 'imaging_bkg_params': {'bh': 128, 'bw': 128, 'fh': 3, 'fw': 3,
+    #                    'pixel_scale': 0.06}, # Imaging background 
+    # 
+    # # Alignment
+    # 'manual_alignment': False,    # Interactive alignment
+    # 'align_clip': 120,            # Parameter for initial alignment pairs
+    # 'align_mag_limits': [14, 24], # Magnitude range for alignment sources
+    # 'align_min_overlap': 0.2,     # Overlap fraction to use HST catalogs
+    # 'align_outlier_threshold': 4, # Parameter for triangle matches
+    # 'align_rms_limit': 2,    # Force shifts = 0 if rms > rms limit
+    # 'align_simple': False,   # ??
+    # 'catalogs': ['PS1','DES','NSC','SDSS','GAIA','WISE'], # Ref cat order
+    # 'gaia_by_date': True,   # Get GAIA catalogs by visit epoch
+    # 'master_radec': None,    # Force reference
+    # 'parent_radec': None,    # Reference to use if no HST overlaps
+    # 
+    # 'run_fine_alignment': False, # Run Fine alignment script
+    # 'fine_radec': None,         # Fine alingment reference,  else GAIA
+    # 
+    # # Image Mosaics                   
+    # 'make_mosaics': True,             # Make full-field mosaics
+    # 'fill_mosaics': 'grism',          # Fill empty parts of IR mosaics
+    # 'mask_spikes': False,             # Mask IR diffraction spikes
+    # 'combine_all_filters': False,      # Combine ACS & IR
+    # 'mosaic_pixel_scale': None,       # Pixel scale of mosaics (0.06)
+    # 'half_optical_pixscale': False,   # Make smaller ACS/UVIS pixel scale
+    # 'mosaic_pixfrac': 0.75,           # Pixfrac of mosaic images
+    # 'reference_wcs_filters': auto_script.VALID_FILTERS, # These define WCS if they exist
+    # 'make_phot': True,                # Make photometric catalog
+    # 
+    # # Spectral Extractions
+    # 'only_preprocess': False, # Make grism models
+    # 'run_extractions': False, # Don't extract grism spectra
+    # 'maglim': [17, 26],       # Magnitude range of grism spectra 
+    # 'run_fit': False          # Fit grism spectra
+    # }
       
     tab = utils.GTable.gread('{0}_footprint.fits'.format(root))
     
@@ -68,55 +74,91 @@ def auto_run(root='j023507-040202', args=[]):
     if not os.path.exists(parent_radec):
         parent_radec = None
     
-    params['parent_radec'] = parent_radec
-    params['master_radec'] = master_radec
+    kwargs['preprocess_args']['parent_radec'] = parent_radec
+    kwargs['preprocess_args']['master_radec'] = master_radec
     
     # Limited filters
-    params['only_preprocess'] = False
-    params['filters'] = auto_script.IR_W_FILTERS + auto_script.IR_GRISMS
-    params['filters'] += auto_script.IR_M_FILTERS
+    kwargs['only_preprocess'] = False
+    kwargs['filters'] = auto_script.IR_W_FILTERS + auto_script.IR_GRISMS
+    kwargs['filters'] += auto_script.IR_M_FILTERS
     
     # Optical filters.  Bluer than F555W often fail for low source counts?
-    params['filters'] += ['F814W', 'F850LP', 'F775W', 'F625W', 'F606W', 'F555W', 'F350LP']
+    kwargs['filters'] += ['F814W', 'F850LP', 'F775W', 'F625W', 'F606W', 'F555W', 'F350LP']
     
-    params['is_parallel_field'] = IS_PARALLEL
-    params['mosaic_pixel_scale'] = 0.06+0.02*params['is_parallel_field']
-    params['mosaic_pixfrac'] = params['mosaic_pixel_scale']/0.12
+    kwargs['is_parallel_field'] = IS_PARALLEL
+    
+    pixel_scale = 0.06+0.02*IS_PARALLEL
+    kwargs['mosaic_args']['wcs_params']['pixel_scale'] = pixel_scale
+    kwargs['mosaic_args']['mosaic_pixfrac'] = pixel_scale/0.12
     
     # Try less aggressive background if CLASH
     # IS_BRIGHT_CLUSTER = utils.column_string_operation(tab['proposal_pi'], ['Postman', 'Lotz'], method='count', logical='or').sum() > 0
     # if IS_BRIGHT_CLUSTER:
     #     print('CLUSTER!')
     # Force conservative background
-    params['imaging_bkg_params'] = {'bh': 256, 'bw': 256, 'fh': 3, 'fw': 3,
-                       'pixel_scale': 0.128} # Imaging background 
+    conservative_background = {'bh': 256, 'bw': 256, 'fh': 3, 'fw': 3,
+                               'pixel_scale': 0.128} # Imaging background
     
-    print('BKG PARAMS: {0}'.format(params['imaging_bkg_params']))
+    kwargs['visit_prep_args']['imaging_bkg_params'] = conservative_background                           
+    
+    print('BKG PARAMS: {0}'.format(conservative_background))
     
     # Command line arguments
     if args:
         for arg in args:
             if arg.startswith('--'):
-                p = arg.strip('--').split('=')[0]
+                pspl = arg.strip('--').split('=')[0]
                 val = arg.split('=')[1]
-                if p in params:
-                    if isinstance(params[p], list):
-                        params[p] = val.split(',')
+                
+                # Split nested dictionaries by '.'
+                if '.' in pspl:
+                    valid = False
+                    ps = pspl.split('.')
+                    d = kwargs
+                    for p in ps:
+                        if p in d:
+                            valid = True
+                            if isinstance(d[p], dict):
+                                d = d[p]
+                else:
+                    d = kwargs
+                    valid = pspl in kwargs
+                    p = pspl
+                
+                print(valid, pspl, d[p])
+                    
+                if valid:
+                    if isinstance(d[p], list):
+                        d[p] = val.split(',')
                         print('Argument {0}: {1}'.format(p, val.split(',')))
                     else:
-                        if isinstance(params[p], bool):
-                            params[p] = val.lower() == 'true'
+                        if isinstance(d[p], bool):
+                            d[p] = val.lower() == 'true'
                         else:
-                            params[p] = params[p].__class__(val)
+                            d[p] = d[p].__class__(val)
                             
-                        print('Argument {0}: {1}'.format(p, params[p]))
-
-    auto_script.go(root=root, HOME_PATH=os.getcwd(), **params)
+                        print('Argument {0}: {1}'.format(p, d[p]))
+                        
+    kwargs_copy = kwargs.copy()
+    auto_script.go(root=root, HOME_PATH=os.getcwd(), **kwargs)
     
     #auto_script.go(root=root, maglim=[19, 23], HOME_PATH=HOME_PATH, inspect_ramps=False, manual_alignment=False, is_parallel_field=IS_PARALLEL, reprocess_parallel=True, only_preprocess=False, run_extractions=False, run_fit=False, s3_sync='cp', fine_radec=None, combine_all_filters=False, gaia_by_date=True, align_simple=False, align_clip=100, master_radec=master_radec, parent_radec=parent_radec, is_dash=False, run_parse_visits=True, reference_wcs_filters=['F160W','F140W','F125W','F105W','F110W','F098M','F814W','F850LP', 'F606W','F435W'])
     
     os.chdir('../Prep/')
     auto_script.make_report(root, make_rgb=True)
+    
+    # Save YAML parameter file
+    fp = open('{0}.run.yaml'.format(root),'w')
+    kwargs_copy['multiband_catalog_args']['phot_apertures'] = None 
+    for k in kwargs_copy: 
+        try: 
+            d = {k:kwargs_copy[k].copy()} 
+        except: 
+            d = {k:kwargs_copy[k]} 
+        
+        yaml.dump(d, stream=fp, default_flow_style=False) 
+    
+    fp.close()
     
     # Done without errors
     os.system('date > /tmp/{0}.success'.format(root))

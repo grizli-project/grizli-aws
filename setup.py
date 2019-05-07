f78955bb20b6a495bc4b86f3bd33c252fe39c28f
Gabriel Brammer <gbrammer@gmail.com>
2019-05-07 07:42:27 +0200

    add version

--- setup.py
+++ setup.py
@@ -1,8 +1,23 @@
 from distutils.core import setup
 import glob
+import subprocess
+
+args = 'git describe --tags'
+p = subprocess.Popen(args.split(), stdout=subprocess.PIPE)
+version = p.communicate()[0].decode("utf-8").strip()
+
+version = '0.1.0'
+
+version_str = """# git describe --tags
+__version__ = "{0}"\n""".format(version)
+
+fp = open('grizli_aws/version.py','w')
+fp.write(version_str)
+fp.close()
+print('Git version: {0}'.format(version))
 
 setup(name='grizli_aws',
-      version='0.1',
+      version=version,
       description='Grizli processing on AWS',
       author='Gabriel Brammer',
       author_email='brammer@stsci.edu',

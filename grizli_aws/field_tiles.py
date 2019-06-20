6a61233c0af6dfa911ed565013ee57c9774fb3a5
Gabriel Brammer <gbrammer@gmail.com>
2019-06-20 12:56:05 +0200

    Fix tile scripts

--- grizli_aws/field_tiles.py
+++ grizli_aws/field_tiles.py
@@ -6,6 +6,9 @@ import glob
 
 from grizli import prep, utils
 
+IR_FILTERS = ['f098m', 'f110w', 'f140w', 'f105w', 'f160w','f125w' ]
+OPTICAL_FILTERS = ['f606w', 'f775w', 'f814w', 'f850lp']
+
 def test_egs():
     
     size=(80,20)
@@ -41,11 +44,7 @@ def tile_parameters(pixscale=0.05, overlap=0.3, tile_size=6):
     fields['egs'] = {'field':'egs', 'ra':214.8288, 'dec':52.8234+dy, 'size':size, 'tile_size':tile_size, 'overlap':overlap, 'pixscale':pixscale, 'theta':theta}
     
     return fields
-    
-       
-IR_FILTERS = ['f098m', 'f110w', 'f140w', 'f105w', 'f160w','f125w' ]
-OPTICAL_FILTERS = ['f606w', 'f775w', 'f814w', 'f850lp']
-    
+        
 def make_candels_tiles(key='gdn', filts=OPTICAL_FILTERS, pixfrac=0.33, output_bucket='s3://grizli-v1/Mosaics/'):
     
     import numpy as np
@@ -78,10 +77,11 @@ def make_candels_tiles(key='gdn', filts=OPTICAL_FILTERS, pixfrac=0.33, output_bu
         dy = np.diff(ylim)[0]*60
         print('\'ra\':{0:.4f}, \'dec\':{1:.4f}, \'size\':({2:.1f}, {3:.1f})'.format(ra, dec, dx, dy))
     
+    if False:
+        from grizli_aws.field_tiles import define_tiles, drizzle_tiles, tile_parameters
+
     fields = tile_parameters(pixscale=0.05, overlap=0.3, tile_size=6)
     
-    from grizli_aws.field_tiles import define_tiles, drizzle_tiles
-     
     tiles = define_tiles(**fields[key])
     
     if False:
@@ -106,22 +106,28 @@ def make_candels_tiles(key='gdn', filts=OPTICAL_FILTERS, pixfrac=0.33, output_bu
     
     drizzle_tiles(all_visits, tiles, filts=filts, prefix=key, pixfrac=pixfrac, output_bucket=output_bucket)
 
-def combine_tile_filters(key='egs'):
+def combine_tile_filters(key='egs', skip_existing=True):
     """
     Make combined images weighted by photflam
     """
     for ix in range(20):
         for iy in range(20):
-            tile_files = glob.glob('{0}-{1:02d}.{2:02d}-*sci.fits*'.format(key, ix, iy))
+            tile_files = glob.glob('{0}-{1:02d}.{2:02d}-*-f*sci.fits*'.format(key, ix, iy))
             if len(tile_files) == 0:
                 continue
             
-            print(tile_files[0])
+            existing = glob.glob('{0}-{1:02d}.{2:02d}-*-ir*sci.fits*'.format(key, ix, iy))
+            existing += glob.glob('{0}-{1:02d}.{2:02d}-*-opt*sci.fits*'.format(key, ix, iy))
+            
+            if (len(existing) > 0) & (skip_existing):
+                continue
+                
+            #print(tile_files[0])
             tile_files.sort()
             
             ####
-            ir_num = None
-            opt_num = None
+            num = {'ir':None, 'opt':None}
+            den = {'ir':None, 'opt':None}
             
             if False:
                 im = pyfits.open('egs-05.02-050mas-f814w_drc_sci.fits.gz')
@@ -135,41 +141,55 @@ def combine_tile_filters(key='egs'):
             ref_h = {}
             ref_h['opt'] = {'INSTRUME': 'ACS', 'DETECTOR': 'WFC', 'PHOTFLAM': 7.0178627203125e-20, 'PHOTBW': 653.24393453125, 'PHOTZPT': -21.1, 'PHOTMODE': 'ACS WFC1 F814W MJD#56438.5725', 'PHOTPLAM': 8045.415190625002, 'FILTER1': 'CLEAR1L', 'FILTER2': 'F814W'}
             ref_h['ir'] = {'INSTRUME': 'WFC3', 'DETECTOR': 'IR', 'PHOTFNU': 9.5291135e-08, 'PHOTFLAM': 1.4737148e-20, 'PHOTBW': 1132.39, 'PHOTZPT': -21.1, 'PHOTMODE': 'WFC3 IR F140W', 'PHOTPLAM': 13922.907, 'FILTER': 'F140W'}
+            
+            output_sci = {}
+            head = {}
+            
+            for sci_file in tile_files:
+                filt_i = sci_file.split('_dr')[0].split('-')[-1]
+                if filt_i in IR_FILTERS:
+                    band = 'ir'
+                else:
+                    band = 'opt'
+                    
+                print(sci_file, band)
+                output_sci[band] = sci_file.replace(filt_i, band)
                 
-            for filt in ['f140w', 'f105w', 'f125w', 'f160w']:
-                sci_file = '{0}-100mas-{1}_drz_sci.fits.gz'.format(key, filt)
-                if not os.path.exists(sci_file):
-                    continue
-
-                print(sci_file)
-
-                #Symlink to individual bands
-                os.system('cp -f {0}-100mas-{2}_drz_sci.fits.gz {1}-{2}_drz_sci.fits.gz'.format(key, root, filt))
-                os.system('cp -f {0}-100mas-{2}_drz_wht.fits.gz {1}-{2}_drz_wht.fits.gz'.format(key, root, filt))
-
                 im_i = pyfits.open(sci_file)
                 wht_i = pyfits.open(sci_file.replace('_sci','_wht'))
                 photflam = im_i[0].header['PHOTFLAM']
-                if num is None:
-                    ref_photflam = photflam*1
-                    ref_h = im_i[0].header
-                    num = im_i[0].data*0
-                    den = num*0
-
+                ref_photflam = ref_h[band]['PHOTFLAM']
+                
+                head[band] = im_i[0].header
+                for k in ref_h[band]:
+                    head[band][k] = ref_h[band][k]
+                    
+                if num[band] is None:
+                    num[band] = im_i[0].data*0
+                    den[band] = num[band]*0
+                    
                 scl = photflam/ref_photflam
                 den_i = wht_i[0].data/scl**2
-                num += im_i[0].data*scl*den_i
-                den += den_i
+                num[band] += im_i[0].data*scl*den_i
+                den[band] += den_i
 
-            sci = num/den
-            wht = den
-            mask = (~np.isfinite(sci)) | (den == 0)
-            sci[mask] = 0
-            wht[mask] = 0
+            for band in ['opt', 'ir']:
+                if num[band] is not None:
+                    sci = num[band]/den[band]
+                    wht = den[band]
+                    
+                    mask = (~np.isfinite(sci)) | (den == 0)
+                    sci[mask] = 0
+                    wht[mask] = 0
 
-            pyfits.PrimaryHDU(data=sci, header=ref_h).writeto('{0}-{1}_drz_sci.fits'.format(root, 'ir'), overwrite=True, output_verify='fix')
-            pyfits.PrimaryHDU(data=wht, header=ref_h).writeto('{0}-{1}_drz_wht.fits'.format(root, 'ir'), overwrite=True, output_verify='fix')
-def make_all_tile_calogs(key='egs', filts=OPTICAL_FILTERS+IR_FILTERS):
+                    print('Write {0}'.format(output_sci[band]))
+                    
+                    pyfits.PrimaryHDU(data=sci, header=head[band]).writeto(output_sci[band], overwrite=True, output_verify='fix')
+                    pyfits.PrimaryHDU(data=wht, header=head[band]).writeto(output_sci[band].replace('_sci', '_wht'), overwrite=True, output_verify='fix')
+            
+            ### TBD - full combination
+            
+def make_all_tile_catalogs(key='egs', threshold=3, filts=OPTICAL_FILTERS+IR_FILTERS):
 
     # Make catalogs
     import os
@@ -179,19 +199,24 @@ def make_all_tile_calogs(key='egs', filts=OPTICAL_FILTERS+IR_FILTERS):
         files = glob.glob('*-[0-9][0-9].[0-9][0-9]*-'+filt_i+'*sci.fits.gz')
         files.sort()
         for i, file in enumerate(files):
+
+            froot = file.split('_dr')[0]
             
             print('\n\n#######\n{0}\n#######\n'.format(froot))
             
-            froot = file.split('_dr')[0]
             if os.path.exists('{0}.cat.fits'.format(froot)):
                 continue
             
-            prep.make_SEP_catalog(froot, threshold=3,
+            try:
+                prep.make_SEP_catalog(froot, threshold=threshold,
                                   rescale_weight=False) 
-            os.system('rm {0}_bkg.fits'.format(froot))
-            #os.system('rm {0}_seg.fits'.format(froot))
-    
-def combine_tile_mosaics(key='gdn', filts=OPTICAL_FILTERS, make_tile_catalogs=False, use_ref='all'):
+                os.system('rm {0}_bkg.fits'.format(froot))
+                #os.system('rm {0}_seg.fits'.format(froot))
+            except:
+                print('\n\n !!!! Failed: ', froot)
+                pass
+                
+def combine_tile_mosaics(key='gdn', filts=OPTICAL_FILTERS, use_ref='all'):
     import os
     import numpy as np
     import matplotlib.pyplot as plt
@@ -276,9 +301,10 @@ def combine_tile_mosaics(key='gdn', filts=OPTICAL_FILTERS, make_tile_catalogs=Fa
         overlap_arcmin = 0.3    
         olap = int(overlap_arcmin*60/coarse_pix*(1+is_fine))
     
-        for ext in ['sci','wht','seg']:
+        extensions = ['sci','wht','seg']
+        for ext in extensions:
             if ext == 'seg':
-                data = np.zeros((sh[0]*ny-olap*(ny-1), sh[1]*nx-olap*(nx-1)), dtype=np.float32)
+                data = np.zeros((sh[0]*ny-olap*(ny-1), sh[1]*nx-olap*(nx-1)), dtype=np.int32)
                 out_ext = 'seg'
             else:
                 data = np.zeros((sh[0]*ny-olap*(ny-1), sh[1]*nx-olap*(nx-1)), dtype=np.float32)
@@ -292,7 +318,7 @@ def combine_tile_mosaics(key='gdn', filts=OPTICAL_FILTERS, make_tile_catalogs=Fa
                 sly = slice(y0,y0+sh[0])
 
                 file_ext = file.replace('drz_sci', out_ext)    
-                file_ext = file_ext.replace('drc_sci', out_ext)    
+                file_ext = file_ext.replace('drc_sci', out_ext.replace('drz','drc'))    
 
                 if (slx.stop > data_sh[1]) | (sly.stop > data_sh[0]):
                     print('skip', file_ext)
@@ -307,7 +333,8 @@ def combine_tile_mosaics(key='gdn', filts=OPTICAL_FILTERS, make_tile_catalogs=Fa
                 data[sly, slx] = sci_i[0].data
         
             mos = pyfits.PrimaryHDU(data=data, header=wh)
-            mos.writeto('{0}-{1:03d}mas-{2}_{3}.fits'.format(key, 100//(1+is_fine), the_filter.lower(), out_ext), overwrite=True)
+            outfile = '{0}-{1:03d}mas-{2}_{3}.fits'.format(key, 100//(1+is_fine), the_filter.lower(), out_ext)
+            mos.writeto(outfile, overwrite=True)
         
             del(data)
     
@@ -315,28 +342,7 @@ def combine_tile_mosaics(key='gdn', filts=OPTICAL_FILTERS, make_tile_catalogs=Fa
     os.system('gzip --force {0}-???mas*fits'.format(key))
     os.system('aws s3 sync --exclude "*" --include "{0}-???mas*gz" ./ s3://grizli-v1/Mosaics/ --acl public-read'.format(key))
     
-    if make_tile_catalogs:
-        # Make catalogs
-        import os
-        from grizli import prep
-        import glob
-        for filt_i in filts:
-            files = glob.glob('*-[0-9][0-9].[0-9][0-9]*-'+filt_i+'*sci.fits.gz')
-            files.sort()
-            for i, file in enumerate(files):
-                
-                print('\n\n#######\n{0}\n#######\n'.format(froot))
-                
-                froot = file.split('_dr')[0]
-                if os.path.exists('{0}.cat.fits'.format(froot)):
-                    continue
-                
-                prep.make_SEP_catalog(froot, threshold=3,
-                                      rescale_weight=False) 
-                os.system('rm {0}_bkg.fits'.format(froot))
-                #os.system('rm {0}_seg.fits'.format(froot))
-                
-def grism_model():
+def grism_prep():
     
     import numpy as np
     from grizli import utils, multifit
@@ -427,8 +433,7 @@ def grism_model():
     os.system('gzip {0}*fits'.format(root))
     os.system('aws s3 sync --exclude "*" --include "{0}*" ./ s3://grizli-v1/Mosaics/ --acl public-read'.format(root))
      
-    ########## Grism prep
-    
+    ########## Grism prep    
     #out_root = 'uds-j021732m0512'
     out_root = root.replace('-grism','')
     
@@ -461,7 +466,27 @@ def grism_model():
             all_info['keep'][ix] = True
             
     np.save(root+'_visits.npy', [visits, groups, all_info[all_info['keep']]])
+
+def grism_model():
+    import numpy as np
+    from grizli import utils, multifit
+    from grizli.pipeline import auto_script
+    import astropy.io.fits as pyfits
+    import os
+    import glob
     
+    from grizli.pipeline import auto_script
+    kwargs = auto_script.get_yml_parameters()
+    
+    if key == 'uds':
+        field_root = root = 'uds-grism-j021732m0512'
+    elif key == 'egs':
+        field_root = root = 'egs-grism-j141956p5255'
+    elif key == 'gds':
+        field_root = root = 'gds-grism-j033236m2748'
+    elif key == 'gdn':
+        field_root = root = 'gdn-grism-j123656p6215'
+
     ####### Run model
     grisms = ['g141', 'g102', 'g800l']
     
@@ -492,7 +517,8 @@ def grism_model():
     for v in visits:
         has_grism = False
         for g in grisms:
-            has_grism |= ('-'+g in v['product'])
+            #has_grism |= ('-'+g in v['product'])
+            has_grism |= v['product'].endswith('-'+g)
 
         if not has_grism:
             continue
@@ -500,9 +526,11 @@ def grism_model():
             print('Add visit {0}'.format(v['product']))
         
         for g in grisms:
-            if '-'+g in v['product']:
+            #if '-'+g in v['product']:
+            if v['product'].endswith('-'+g):
                 grism_files[g].extend(v['files'])        
-                 
+        
+        # Copy file to local dir         
         for aws, f in zip(v['awspath'], v['files']):
             if os.path.exists(f):
                 continue
@@ -510,8 +538,8 @@ def grism_model():
                 os.system('aws s3 cp s3://{0}/{1} .'.format(aws, f))
             
     for g in grisms:
-        os.chdir('../Prep')
         print(g, len(grism_files[g]), len(np.unique(grism_files[g])))
+        os.chdir('../Prep')
         grism_files[g].sort()
         
         kwargs['grism_prep_args']['files'] = grism_files[g]
@@ -533,15 +561,56 @@ def grism_model():
         # Make drizzle model images
         grp.drizzle_grism_models(root=root, kernel='point', scale=0.15)
         del(grp)
-        os.system('aws s3 sync --exclude "*" --include "{0}*_grism*" ./ s3://grizli-v1/GrismMosaics/ --acl public-read'.format(key))
-        os.system('aws s3 sync --exclude "*" --include "*GrismFLT*" --include "*wcs.fits" ./ s3://grizli-v1/GrismMosaics/ --acl public-read'.format(key))
+        os.system('aws s3 sync --exclude "*" --include "{0}*_grism*" ./ s3://grizli-v1/Pipeline/{0}/Extractions/ --acl public-read'.format(root))
+        os.system('aws s3 sync --exclude "*" --include "*GrismFLT*" --include "*wcs.fits" ./ s3://grizli-v1/Pipeline/{0}/Extractions/ --acl public-read'.format(root))
     
-    
+    if False:
+        # Copy gds g141 files to correct directory
+        for i, file in enumerate(grism_files['g141']):
+            print('# {0} {1}'.format(i, file))
+            os.system('aws s3 sync --exclude "*" --include "{0}*" s3://grizli-v1/GrismMosaics/ s3://grizli-v1/Pipeline/{1}/Extractions/'.format(file.split('_fl')[0], root))
+        
+        # Wrong directory gdn >> gds
+        old = 's3://grizli-v1/Pipeline/gdn-grism-j123656p6215/Extractions'
+        new = 's3://grizli-v1/Pipeline/gds-grism-j033236m2748/Extractions'
+        
+        for g in grisms:
+            for i, file in enumerate(grism_files[g]):
+                filer = file.split('_fl')[0]
+                files = glob.glob(filer+'*')
+                if len(files) > 0:
+                    print('# {0} {1}'.format(i, file))
+                    for file_i in files:
+                        print('  '+file_i)
+                        os.system('mv {0} ../../gds-grism-j033236m2748/Prep/'.format(file_i))
+                        os.system('aws s3 mv {0}/{2} {1}/{2} --acl public-read'.format(old, new, file_i))
+        
+        # Check missing
+        os.system('aws s3 ls s3://grizli-v1/Pipeline/{0}/Extractions/ |grep GrismFLT.fits | sed "s/.01.GrismFLT.fits//" | awk \'{{{{print $4}}}}\' > {1}.files'.format(root, key))
+        remote = np.loadtxt('{0}.files'.format(key), dtype=str)
+        all_files = []
+        for g in grism_files:
+            print('---'+g)
+            missing = []
+            for f in grism_files[g]:
+                if f.split('_fl')[0] not in remote:
+                    missing.append(f)
+            
+            print('    Missing: '+' '.join(missing))
+            all_files.extend(grism_files[g])
+            
+        extra = []
+        for f in remote:
+            if f+'_flt.fits' not in all_files:
+                print('Extra: '+f)
+                
     ################
       
     # Extractions
+    MW_EBV = {'gdn':0.011, 'gds':0.007, 'egs': 0.007, 'uds':0.019}
+    
     pline = auto_script.DITHERED_PLINE
-    auto_script.generate_fit_params(field_root=root, prior=None, MW_EBV=0.019, pline=pline, fit_only_beams=True, run_fit=True, poly_order=7, fsps=True, min_sens=0.001, sys_err=0.03, fcontam=0.2, zr=[0.05, 3.4], save_file='fit_args.npy', fit_trace_shift=False, include_photometry=True, use_phot_obj=False)
+    auto_script.generate_fit_params(field_root=root, prior=None, MW_EBV=MW_EBV[key], pline=pline, fit_only_beams=True, run_fit=True, poly_order=7, fsps=True, min_sens=0.001, sys_err=0.03, fcontam=0.2, zr=[0.05, 3.4], save_file='fit_args.npy', fit_trace_shift=False, include_photometry=True, use_phot_obj=False)
     
     ids = [27115]
     auto_script.extract(field_root=root, maglim=[13, 24], prior=None, MW_EBV=0.019, ids=ids, pline=pline, fit_only_beams=True, run_fit=False, poly_order=7, oned_R=30, master_files=None, grp=grp, bad_pa_threshold=None, fit_trace_shift=False, size=32, diff=True, min_sens=0.01, fcontam=0.2, min_mask=0.01, sys_err=0.03, skip_complete=True, args_file='fit_args.npy', get_only_beams=False)
--- grizli_aws/version.py
+++ grizli_aws/version.py
@@ -1,2 +1,2 @@
 # git describe --tags
-__version__ = "0.1.0-103-g4d3cff4"
+__version__ = "0.1.0-104-gd1ad555"

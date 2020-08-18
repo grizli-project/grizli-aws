3937fe454015d5a0412bd5a9c45aa6358d25c1d2
Gabriel Brammer <gbrammer@gmail.com>
2020-08-18 16:36:46 +0200

    pass args

--- scripts/irac_model_single.sh
+++ scripts/irac_model_single.sh
@@ -9,6 +9,6 @@ aws s3 cp ${root}.start.txt s3://grizli-v1/IRAC/Log/MStart/ --acl public-read
 
 aws s3 cp s3://grizli-v1/IRAC/Log/MFinished/${root}.finished.txt /tmp/ --acl public-read
 
-irac_model_single.py ${root}
+irac_model_single.py $@
 
 aws s3 cp /tmp/${root}.finished.txt s3://grizli-v1/IRAC/Log/MFinished/ --acl public-read

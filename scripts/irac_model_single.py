4e8fc6332a5ab970c3e9c5b6e60cd621eec1ce86
Gabriel Brammer <gbrammer@gmail.com>
2020-08-18 17:06:28 +0200

    dump parameters

--- scripts/irac_model_single.py
+++ scripts/irac_model_single.py
@@ -7,30 +7,18 @@ import json
 
 import golfir.model
 import golfir.utils
+import yaml
 
-def run(root, argv=[], ds9=None):
-    
-    run_dir = '/GrizliImaging/{0}'.format(root)
-    if os.path.exists(run_dir):
-        print('directory {0} exists'.format(run_dir))
-        return True
-
-    if os.path.exists('/tmp/{0}.finished.txt'.format(root)):
-        print('/tmp/{0}.finished.txt'.format(root))
-        return True
-    
-    #ds9 = None
-    
-    if ds9 is 'connect':
-        import grizli.ds9
-        ds9 = grizli.ds9.DS9()
+def run(root, argv=[]):
         
-    defaults = {'ds9': ds9, 
+    #ds9 = None
+            
+    defaults = {'ds9': None, 
               'patch_arcmin': 1.0,      # Size of patch to fit
               'patch_overlap': 0.2,     # Overlap of automatic patches
               'mag_limit': [24, 27],    # Two-pass modeling.  Fit sources brighter than mag_limit in HST catalog
               'run_alignment': True,    # Run fine alignment between IRAC-HST, in between two steps of `mag_limit`
-              'galfit_flux_limit': 70,  # Brightness limit (uJy) of objects to fit with GALFIT
+              'galfit_flux_limit': -50,  # Brightness limit (uJy) of objects to fit with GALFIT.  Or S/N if negative
               'refine_brightest': True, # Refine masked bright objects with galfit
               'any_limit': 16,          # Brightness limit below which to mask *any* sources
               'point_limit': 16,        # Brightness limit below which to mask point-like sources
@@ -44,11 +32,30 @@ def run(root, argv=[], ds9=None):
               'PATH': '/GrizliImaging/', 
               'use_patches': True, 
               'sync_results': True, 
-              'clean_PATH': True}
+              'clean_PATH': True, 
+              'skip_if_exists': True}
           
-
+              
     defaults['patch_arcmin'] = -1
     args, kwargs = golfir.utils.argv_to_dict(argv, defaults=defaults)
+    
+    run_dir = os.path.join(kwargs['PATH'], root)
+    if os.path.exists(run_dir) & kwargs['skip_if_exists']:
+        print('directory {0} exists'.format(run_dir))
+        return True
+
+    if os.path.exists('/tmp/{0}.finished.txt'.format(root)):
+        print('/tmp/{0}.finished.txt'.format(root))
+        return True
+    
+    with open(os.path.join(run_dir, root + '.golfir.yml'), 'w') as fp:
+        yaml.dump(kwargs)
+        
+    if kwargs['ds9'] == 'connect':
+        import grizli.ds9
+        print('Use DS9!')
+        
+        kwargs['ds9'] = grizli.ds9.DS9()
     
     golfir.model.run_all_patches(root, **kwargs)
         
